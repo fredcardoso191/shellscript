27b096a9151e757cda15bae5bb4c99d58b48d6c9
Fred Cardoso <fredcardoso01@hotmail.com>
2022-04-22 15:43:24 -0300

    ScriptsAutomacao

--- ScriptsAutomacao/cpp.sh
+++ ScriptsAutomacao/cpp.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
+main() {
+	echo "
+#include <iostream>
+
+int main() {
+	return 0;
+}" > $file.cpp;
+}
+
 cd Workspace/cpp;
 
 echo "Enter the project name: ";
 read project;
 
 mkdir $project;
-cd $project
+cd $project;
 
 echo "Create first file? (Y/N) ";
-read decision;
+read first_file;
 
-if [ $decision == Y ] || [ $decision == y ]
+if [ $first_file == Y ] || [ $first_file == y ]
 then
 	echo "Enter the file name: ";
 	read file;
-	touch $file.cpp;
+
+	echo "Create default project? (Y/N) ";
+	read default;
+
+	if [ $default == Y ] || [ $default == y ]
+	then
+		main;
+	else
+		echo > $file.cpp;
+	fi
 fi
 
 code .;
